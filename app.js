b62a6ecf75b3cd7e47c241f7cf9c9accf78a822d
gary <gary@ligginsdevelopment.com>
2021-03-25 14:59:51 -0700

    everything working, adding css later

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const app = express();
 const Post = require('./api/models/posts');
+var multer = require('multer');
 const postsData = new Post();
+var storage = multer.diskStorage({
+    destination: function (req, file, cb)
+ {
+     cb(null, "./uploads")
+ },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+
+
+const getExt = (mimetype) => {
+    switch(mimetype){
+        case "image/png":
+            return '.png';
+        case "image/jpeg":
+            return '.jpg';
+    }
+}
+
+var upload = multer({ storage: storage });
+
+
+
+app.use(express.json())
 
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
@@ -27,6 +53,18 @@ app.get("/api/posts/:post_id", (req,res) => {
     } else {
         res.status(404).send("not found")
     }
+})
+
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title ,
+        "content": req.body.content ,
+        "post_image": req.file.path,
+        "added_date": `${Date.now()}`
+    }
+    postsData.add(newPost)
+    res.status(201).send(newPost);
 })
 
 app.listen(3000, ()=> console.log('listening on port 3000'))
